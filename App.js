98de8fdf73f6a9f49b872d364d1c4c22e9b4ff64
hamzahashmi033 <hashmihamza846@gmail.com>
2024-03-22 02:05:38 +0500

    Add styling in a Drawer Navigation and now switching towards bottom navigation

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import WelcomeScreen from './screen/WelcomeScreen';
-import UserScreen from './screen/UserScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import WelcomeScreen from "./screen/WelcomeScreen";
+import UserScreen from "./screen/UserScreen";
+import { Ionicons } from "@expo/vector-icons";
+
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Drawer.Navigator>
-      <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-      <Drawer.Screen name="User" component={UserScreen} />
-    </Drawer.Navigator>
-  </NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3c0a6b" },
+          headerTintColor: "white",
+          drawerActiveBackgroundColor: "#f0e1ff",
+          drawerActveTintColor: "#3c0a6b",
+          // drawerStyle: { backgroundColor: 'black' },
+        }}
+      >
+        <Drawer.Screen
+          name="Welcome"
+          options={{
+            drawerLabel: "Welcome Screen",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+          }}
+          component={WelcomeScreen}
+        />
+        <Drawer.Screen
+          name="User"
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+          component={UserScreen}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
